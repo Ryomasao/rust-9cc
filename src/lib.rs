6011f79f9c3fa630a83d714ac040850afe317d52
Ryomasao <konoemario@gmail.com>
2021-09-29 10:00:28 +0900

    typo

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,8 @@ use std::path::Path;
 
 #[derive(Debug)]
 pub struct Config {
-	pub filename: String,
+	pub entry: String,
+	pub dist: String,
 }
 
 // 組み込みのエラーはいろいろ存在していて、1関数内に複数エラーの型が存在していると
@@ -23,24 +24,35 @@ type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 // https://doc.rust-jp.rs/rust-by-example-ja/error/multiple_error_types/wrap_error.html
 // 組み込みErrorを独自エラー型に変換するのがめんどくさそうね
 
-impl Config {
-	pub fn new(mut args: env::Args) -> Result<Config> {
-		args.next();
-		// lenってnextイテレータすすめると減るのかな
-		if args.len() != 1 {
-			return Err("引数の数が間違ってるよ".into());
-		}
+impl<'a> Config {
+	// @MEMO デフォルトパラメータみたいなことができれば
+	// @MEMO const値なんだけど、ライフライムを指定する意味はあるのかな
+	const DEFAULT_ENTRY: &'a str = "tmp.c";
+	const DEFAULT_DIST: &'a str = "tmp.s";
 
-		let filename = match args.next() {
+	pub fn new(mut args: env::Args) -> Config {
+		args.next();
+		// @MEMO lenってnextイテレータすすめると減るのかな
+		//if args.len() != 2 {
+		//	return Err("引数の数が間違ってるよ".into());
+		//}
+
+		let entry = match args.next() {
 			Some(args) => args,
-			None => return Err("コンパイル対象のファイル名を入力してね".into()),
+			// @MEMO Copyになるけどいいかな
+			None => Self::DEFAULT_ENTRY.to_string(),
 		};
 
-		Ok(Config { filename })
+		let dist = match args.next() {
+			Some(args) => args,
+			None => Self::DEFAULT_DIST.to_string(),
+		};
+
+		Config { entry, dist }
 	}
 }
 
-pub fn run(config: Config) -> Result<()> {
+pub fn run(config: &Config) -> Result<()> {
 	//
 	// ファイル読み込み
 	//
@@ -52,7 +64,7 @@ pub fn run(config: Config) -> Result<()> {
 	// 上記のショートカット
 	// 厳密にはエラー時に返す型がちょっと違うみたい
 	// https://doc.rust-jp.rs/book-ja/ch09-02-recoverable-errors-with-result.html#%E3%82%A8%E3%83%A9%E3%83%BC%E5%A7%94%E8%AD%B2%E3%81%AE%E3%82%B7%E3%83%A7%E3%83%BC%E3%83%88%E3%82%AB%E3%83%83%E3%83%88-%E6%BC%94%E7%AE%97%E5%AD%90
-	let mut f = File::open(config.filename)?;
+	let mut f = File::open(&config.entry)?;
 	let mut contents = String::new();
 	f.read_to_string(&mut contents)?;
 	println!("contents:{}", contents);
@@ -67,7 +79,7 @@ pub fn run(config: Config) -> Result<()> {
 	//
 	// コンパイル結果を出力
 	//
-	output(&compiled)?;
+	output(&compiled, &config)?;
 	Ok(())
 }
 
@@ -75,20 +87,19 @@ fn builder(mut s: String) -> Result<String> {
 	s.push_str(".intel_syntax noprefix\n");
 	s.push_str(".globl main\n");
 
-	s.push_str("main\n");
+	s.push_str("main:\n");
 	// https://doc.rust-lang.org/std/macro.write.html
 	// format!マクロで文字列を生成すると、生成した文字列をヒープに書き込んだ後
 	// sのヒープにコピーして、生成した文字列をdropすることになる
 	// writeを使えば、sのヒープに直接formatした文字列を書き込めるってことかな
 	// https://users.rust-lang.org/t/how-do-i-push-str-the-contents-of-a-variable/45594/6
-	write!(&mut s, "  mov rax {}\n", 12)?;
+	write!(&mut s, "  mov rax, {}\n", 12)?;
 	s.push_str("  ret\n");
 	Ok(s)
 }
 
-fn output(s: &str) -> Result<()> {
-	const DIST: &str = "test.cc";
-	let path = Path::new(DIST);
+fn output(s: &str, config: &Config) -> Result<()> {
+	let path = Path::new(&config.dist);
 	let mut file = File::create(&path)?;
 	file.write_all(s.as_bytes())?;
 	Ok(())
--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,16 @@ use rust9cc::run;
 use rust9cc::Config;
 
 fn main() {
-    let config = Config::new(env::args()).unwrap_or_else(|err| {
-        eprintln!("引数のパースに失敗! :{}, 引数:{:?}", err, env::args());
-        process::exit(1);
-    });
+    // Configパースが必要になったら復活する
+    //let config = Config::new(env::args()).unwrap_or_else(|err| {
+    //    eprintln!("引数のパースに失敗! :{}, 引数:{:?}", err, env::args());
+    //    process::exit(1);
+    //});
 
-    if let Err(e) = run(config) {
-        eprintln!("コンパイルに失敗! {}:", e);
+    let config = Config::new(env::args());
+
+    if let Err(e) = run(&config) {
+        eprintln!("コンパイルに失敗! \n config:{:#?}\n error:{} \n", config, e);
         process::exit(1);
     }
 }
